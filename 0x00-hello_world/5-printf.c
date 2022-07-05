6918b8555a1e26c157deafb0b8ff96ad24be34d2
SULA-MAN00 <sulama4000@gmail.com>
2022-07-04 17:31:02 -0700

    low

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: 0 number
- *     */
+* main - Entry point
+*
+* Return: 0 number
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
