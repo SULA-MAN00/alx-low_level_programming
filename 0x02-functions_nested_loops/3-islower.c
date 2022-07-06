9f30d5d3ff620c3c2de967f9d452cd9c9b285c3c
SULA-MAN00 <sulama4000@gmail.com>
2022-07-06 15:40:59 -0700

    nested

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,20 @@
 #include "main.h"
-
 /**
-* main - check the code.
+* _islower - function
 *
-* Return: Always 0.
+* @c: The character to evaluate
+*
+* Return: Get 1 if c is uppercase,
+* get 0 otherwise
 */
-
 int _islower(int c)
 {
 		if (c >= 97 && c <= 122)
 		{
 		      return (1);
-					}
+                }
 		else
 		{
 		      return (0);
-				    }
+		}
 }
