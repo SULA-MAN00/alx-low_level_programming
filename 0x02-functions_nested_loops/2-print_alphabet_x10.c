3fed6e2a20036a5aca9f0dab52693045d444581f
SULA-MAN00 <sulama4000@gmail.com>
2022-07-06 17:02:53 -0700

    nested

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,18 +5,18 @@
 */
 void print_alphabet_x10(void)
 {
-		char a = 'a';
-		int n = 10;
+	char a = 'a';
+	int n = 10;
 
-		while (n >= 1)
-		{
+	while (n >= 1)
+	{
 		while (a <= 'z')
 		{
-		  _putchar(a);
-		  ++a;
-			}
-		  --n;
-		  _putchar(10);
-		  a = 'a';
+			_putchar(a);
+			++a;
 		}
+		--n;
+		_putchar(10);
+		a = 'a';
+	}
 }
