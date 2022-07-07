ebc95c8f1b07b8a2756caf8d644d3697d3f22eaf
SULA-MAN00 <sulama4000@gmail.com>
2022-07-06 17:14:26 -0700

    nested

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
+* main - check the code
 *
-* print_alphabet - print the alphabet
-*
+* Return: Always 0.
 */
 void print_alphabet(void)
 {
@@ -13,7 +13,6 @@ void print_alphabet(void)
 		_putchar(a);
 		++a;
 	}
-	
 	_putchar(10);
 
 }
