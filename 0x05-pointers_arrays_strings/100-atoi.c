b77ae6dd6e01c1f5a74621239f82ff9ea7f719fb
SULA-MAN00 <sulama4000@gmail.com>
2022-07-12 13:18:54 -0700

    pointer-array

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 * _atoi - Entry point
 *
@@ -9,5 +10,18 @@
 */
 int _atoi(char *s)
 {
-	return (0);
+	int negative = 1, i = 0;
+	unsigned int numero = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+			negative *= -1;
+		else if (s[i] >= 0 + '0' && s[i] < 10 + '0')
+			numero = numero * 10 + (s[i] - '0');
+		else if (s[i - 1] >= 0 + '0' && s[i - 1] < 10 + '0')
+			break;
+	}
+	return (numero *negative);
+
 }
