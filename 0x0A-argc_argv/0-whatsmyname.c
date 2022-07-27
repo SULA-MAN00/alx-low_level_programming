7ba071258b6e073c0612b1f1c0a5a91c8015c2b5
SULA-MAN00 <sulama4000@gmail.com>
2022-07-27 10:50:07 -0700

    argcrgv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,12 +8,10 @@
 * 
 * Return: always 0
 */
-
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	if (argc == 1)
-	{
-		printf("%s\n", *argv);
-	}
+	printf("%s\n", *argv);
+
 	return (0);
+
 }
