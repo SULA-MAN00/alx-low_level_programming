1fb51aea5be477094101e54dfb981b2b62db1262
SULA-MAN00 <sulama4000@gmail.com>
2022-07-27 14:26:38 -0700

    argcargv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,37 +1,89 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
-* main - print the multiplication of two arguments
-* @argc: has the length of the arguments
-* @argv: has the arguments
-* Return: 0 to indicate a good working of the program
+* _atoi - converts a string to an integer
+* @s: string to be converted
+*
+* Return: the int converted from the string
 */
 
-int main(int argc, char **argv)
+int _atoi(char *s)
 {
-	int i, j, num = 0;
+	int i, d, n, len, f, digit;
 
-	
-	if (argc < 3)
-		printf("0\n");
-	else
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
-		for (i = 1; i < argc; i++)
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			for (j = 0; (argv[i])[j] != '\0'; j++)
-			{
-				if (((argv[i])[j] - '0') >= 0 && ((argv[i])[j] - '0') <= 9)
-					continue;
-				else
-				{
-					printf("Error\n");
-					return (1);
-				}
-			}
-			num += atoi(argv[i]);
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		printf("%d\n", num);
+		i++;
 	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+* main - adds two positive number
+* @argc: number of arguments
+* @argv: array of arguents
+*
+* Return: 0 (Success), or 1 (Success)
+*/
+
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j, k;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = _atoi(argv[k]);
+
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+	
+	printf("%d\n", sum);
+
 	return (0);
 }
