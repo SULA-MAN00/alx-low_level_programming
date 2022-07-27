7d76b7b51e9771ea6dfff59d15cac46aa3eb166e
SULA-MAN00 <sulama4000@gmail.com>
2022-07-27 10:41:21 -0700

    argcargv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,8 @@
 * main - print the name of this function
 * @argc: has the length of the arguments
 * @argv: has the arguments
-* Return: 0 to indicate a good working of the program
+* 
+* Return: always 0
 */
 
 int main(int argc, char **argv)
